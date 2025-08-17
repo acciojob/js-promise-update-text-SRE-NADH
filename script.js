192818ec9ae8bd18f2f39261a612ccbcf3ee5e23
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2025-08-17 17:58:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let output = document.getElementById('output');
+
+const updatePromise = ()=>{
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		}, 1000)
+	})
+}
+
+
+updatePromise().then((data)=>{
+	// console.log(data);
+	output.innerText = data;
+})
